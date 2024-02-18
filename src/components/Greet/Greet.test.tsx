315bee2824ffc24c9dd7b3616cfaf3ab91d30ce4
Denys Bokov <denisbokov1703@gmail.com>
2024-02-18 15:42:47 +0100

    Jest testing with nasted describe module implemented

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,14 +1,20 @@
-import { Greet } from "./Greet"
-import { render, screen } from "@testing-library/react"
+import { Greet } from "./Greet";
+import { render, screen } from "@testing-library/react";
 
-test('Greet renders corrected', () => {
-  render(<Greet />);
-  const textElement = screen.getByText('JEST');
-  expect(textElement).toBeInTheDocument();
-});
+describe('Greet', () => {
+  test('renders correctly', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('JEST');
+    expect(textElement).toBeInTheDocument();
+  })
 
-test('Greet with the props name', () => {
-  render(<Greet name="Denys Bokov"/>)
-  const textElement = screen.getByText('Denys Bokov JEST')
-  expect(textElement).toBeInTheDocument();
+  describe('Greet - name', () => {
+    test('render with the name', () => {
+      const testName = 'DB';
+      render(<Greet name={testName}/>);
+
+      const textNameElement = screen.getByText('DB JEST');
+      expect(textNameElement).toBeInTheDocument();
+    })
+  });
 })

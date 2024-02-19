e622d7302e1588aef5e5434911e23265a9b14e16
Denys Bokov <denisbokov1703@gmail.com>
2024-02-19 20:13:44 +0100

    replaced nameElement test with getByPlaceholderText matcher

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { Form } from "./Form";
 
-describe.only('Form', () => {
+describe('Form', () => {
   test('TextInput - renders correctly', () => {
     render(<Form />);
 

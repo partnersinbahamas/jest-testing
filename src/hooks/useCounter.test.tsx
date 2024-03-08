cd4cdd86a79c47a13cb60b9742c047c770fb168d
Denys Bokov <denisbokov1703@gmail.com>
2024-03-08 18:47:51 +0100

    tested useCounter hook with increment & decrement func`s using act()

--- src/hooks/useCounter.test.tsx
+++ src/hooks/useCounter.test.tsx
@@ -1,3 +1,4 @@
+import { act } from "react-dom/test-utils";
 import { renderHook } from "@testing-library/react";
 import { useCounter } from "./useCounter";
 
@@ -9,5 +10,17 @@ describe('useCounter', () => {
   it('renders with initialState', () => {
     const { result } = renderHook(useCounter, {initialProps: { initialCount: 10 }});
     expect(result.current.count).toBe(10);
+  });
+  it('renders with increment functions', () => {
+    const { result } = renderHook(useCounter);
+
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(1);
+  });
+  it('renders count with decrement function', () => {
+    const { result } = renderHook(useCounter);
+
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(-1);
   });
 })
